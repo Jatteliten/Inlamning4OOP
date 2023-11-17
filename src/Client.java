3dc63d33ecc6ec4c226f865d9619999c411ba2f6
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2023-11-17 06:28:57 +0100

    Added functionality to send questions and receive answers to and from multiple clients

--- src/Client.java
+++ src/Client.java
@@ -1,16 +1,26 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
     int port = 12344;
     String ip = "127.0.0.1";
-    PrintWriter pw;
+    PrintWriter out;
+    BufferedReader in;
+    String question;
 
     public Client() {
         try(Socket socketToServer = new Socket(ip, port)) {
-            pw = new PrintWriter(socketToServer.getOutputStream(), true);
+            out = new PrintWriter(socketToServer.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socketToServer.getInputStream()));
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
 
+            while((question = in.readLine()) != null){
+                System.out.println(question);
+                out.println(userInput.readLine());
+            }
         } catch(IOException e) {
             throw new RuntimeException(e);
         }
--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,17 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Server extends Thread {
     Socket serverSocket;
+    String[] questions = new String[]{"1+1=", "1+2=", "1+3=", "1+4=", "1+5="};
+    String[] answers = new String[]{"2", "3", "4", "5", "6"};
+    String answer;
+    int counter = 0;
 
     public Server(Socket s){
         this.serverSocket = s;
@@ -10,7 +19,20 @@ public class Server extends Thread {
 
     @Override
     public void run(){
+        try(BufferedReader in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(serverSocket.getOutputStream(), true)){
 
+            while (true){
+                out.println(questions[counter%5]);
+                answer = in.readLine();
+                if(answer.equalsIgnoreCase(answers[counter%5])){
+                    counter++;
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
