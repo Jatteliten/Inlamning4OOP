14f4ec903b9a82c596be15a5ba95bd7a21b628f1
Farzad <farzad_jalili@hotmail.se>
2023-11-21 20:45:18 +0100

    Gjorde om protokollet igen, implementera logik för att hantera flera spelare, frågor/kategori och poäng.

--- src/Server/Protokoll.java
+++ src/Server/Protokoll.java
@@ -1,76 +1,135 @@
 package Server;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Protokoll {
     private enum GameState {
         INITIAL,
-        PLAYER_1_ENTRY,
-        PLAYER_2_ENTRY,
+        PLAYER_ENTRY,
+        NEW_GAME,
         CATEGORY_SELECTION,
         QUESTION,
         FINISHED,
     }
 
     private GameState state = GameState.INITIAL;
-    private List<String> categories = List.of("Kategori 1", "Kategori 2", "Kategori 3");
+    private String currentPlayerName;
+    private List<String> players = new ArrayList<>();
+    private String currentCategory;
+    private List<String> categories = List.of("Category1", "Category2", "Category3");
     private List<String> currentQuestions;
     private int questionCounter = 0;
-    private String player1Name;
-    private String player2Name;
-    private int player1Score = 0;
-    private int player2Score = 0;
+    private int currentPlayerIndex = 0; // Håller koll på vilken spelare som är aktiv
+    private List<Integer> playerScores = new ArrayList<>();
+
+    public Protokoll() {
+        players.add("Player1");
+        players.add("Player2");
+        playerScores.add(0);
+        playerScores.add(0);
+    }
 
     public String processUserInput(String userInput) {
         switch (state) {
             case INITIAL:
-                state = GameState.PLAYER_1_ENTRY;
-                return "Väntar på spelare 1 att ange sitt namn.";
+                state = GameState.PLAYER_ENTRY;
+                return "Welcome [String]";
 
-            case PLAYER_1_ENTRY:
-                player1Name = userInput;
-                state = GameState.PLAYER_2_ENTRY;
-                return "name " + player1Name + " ansluten. Väntar på spelare 2 att ange sitt namn.";
+            case PLAYER_ENTRY:
+                if (currentPlayerIndex < players.size()) {
+                    currentPlayerName = userInput;
+                    state = GameState.NEW_GAME;
+                    return "name " + currentPlayerName;
+                } else {
+                    state = GameState.CATEGORY_SELECTION;
+                    currentPlayerIndex = 0;
+                    return "new game [String]";
+                }
 
-            case PLAYER_2_ENTRY:
-                player2Name = userInput;
+            case NEW_GAME:
                 state = GameState.CATEGORY_SELECTION;
-                return "name " + player2Name + " ansluten. Välj en kategori: " + String.join(", ", categories);
+                return "new game [String]";
 
             case CATEGORY_SELECTION:
                 state = GameState.QUESTION;
-                currentQuestions = getQuestionsForCategory(userInput);
-                return "Question " + currentQuestions.get(questionCounter);
+                setCurrentQuestions(userInput);
+                return "Questions " + currentQuestions.get(questionCounter);
 
             case QUESTION:
-                if (questionCounter < 3) {
+                if (questionCounter < 4) {
                     String currentQuestion = currentQuestions.get(questionCounter);
                     questionCounter++;
-                    return "Question " + currentQuestion;
+                    return "Questions " + currentQuestion;
                 } else {
                     state = GameState.FINISHED;
-                    return "Inga fler frågor. Väntar på resultatet...";
+                    return "points " + playerScores.get(currentPlayerIndex);
                 }
 
             case FINISHED:
-                // + logik för att beräkna resultatet
-                String resultMessage = "results Resultatet är klart.\n" +
-                        player1Name + ": " + player1Score + " poäng\n" +
-                        player2Name + ": " + player2Score + " poäng";
-                return resultMessage;
+
+                playerScores.set(currentPlayerIndex, playerScores.get(currentPlayerIndex) + Integer.parseInt(userInput));
+
+                currentPlayerIndex++;
+                if (currentPlayerIndex < players.size()) {
+                    currentPlayerName = players.get(currentPlayerIndex);
+                    state = GameState.CATEGORY_SELECTION;
+                    questionCounter = 0;
+                    return "new game [String]";
+                } else {
+                    String resultMessage = "results Resultatet är klart.\n";
+                    for (int i = 0; i < players.size(); i++) {
+                        resultMessage += players.get(i) + ": " + playerScores.get(i) + " poäng\n";
+                    }
+                    return resultMessage;
+                }
 
             default:
                 return "Ogiltigt tillstånd.";
         }
     }
 
+    private void setCurrentQuestions(String selectedCategory) {
+        // Implementera logik för att hämta frågor baserat på vald kategori
+        // Exempel: this.currentQuestions = getQuestionsForCategory(selectedCategory);
+        if (questionCounter == 0) {
+            // Vid första frågan i varje rond, sätt ny kategori och frågor
+            currentCategory = selectedCategory;
+            this.currentQuestions = getQuestionsForCategory(currentCategory);
+        }
+    }
     private List<String> getQuestionsForCategory(String category) {
-        // implementera logik för att hämta fråggor baserat på kategorin
+        List<String> questions = new ArrayList<>();
 
-        return List.of("Fråga 1", "Fråga 2", "Fråga 3");
+        try (BufferedReader reader = new BufferedReader(new FileReader("CategoriesAndQuestions.txt"))) {
+            String line;
+            boolean inDesiredCategory = false;
+
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("Category") && line.contains(category)) {
+                    inDesiredCategory = true;
+                } else if (line.startsWith("Category")) {
+                    inDesiredCategory = false;
+                } else if (inDesiredCategory && line.startsWith("Fråga")) {
+                    questions.add(line.trim());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return questions;
+    }
+
+
+    public void handlePoints(String userInput) {
+        if (state == GameState.QUESTION) {
+            int points = Integer.parseInt(userInput);
+            playerScores.set(currentPlayerIndex, playerScores.get(currentPlayerIndex) + points);
+            state = GameState.CATEGORY_SELECTION;
+        }
     }
 }
