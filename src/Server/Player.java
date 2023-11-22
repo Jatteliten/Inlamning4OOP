36028d61f8c69cf894d1e945500e714b2e44fa16
Elias Elamri <elias.elamri@yh.nackademin.se>
2023-11-22 18:22:02 +0100

    Lagt till kod i player klassen för hantering av poäng Lagt till kod i server klassen för hantering av poäng

--- src/Server/Player.java
+++ src/Server/Player.java
@@ -1,26 +1,58 @@
 package Server;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Player {
     String name;
     int score = 0;
     ObjectOutputStream objectOutputStream;
+    ObjectInputStream objectInputStream;
     boolean playerOne;
 
-    public Player(ObjectOutputStream os, String name){
+
+    //ObjectOutputStream används för att skicka poäng från spelaren till servern via sendScoreToServer-metoden.
+   // ObjectInputStream används för att ta emot poäng från servern via receiveScoreFromServer-metoden.
+    public Player(ObjectOutputStream os, ObjectInputStream is, String name) {
         this.objectOutputStream = os;
+        this.objectInputStream = is;
         this.name = name;
     }
+
+    public void sendScoreToServer() {
+        try {
+            objectOutputStream.writeObject(score);
+            objectOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Metod för att ta emot poäng från servern
+    public void receiveScoreFromServer() {
+        try {
+            int receivedScore = (int) objectInputStream.readObject();
+            // Här kan man hantera de mottagna poängen
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     public int getScore() {
         return score;
     }
+
     public void addScore(int scoreToAdd) {
         this.score = score + scoreToAdd;
     }
 
     public ObjectOutputStream getObjectOutputStream() {
         return objectOutputStream;
+    }
+
+    public ObjectInputStream getObjectInputStream() {
+        return objectInputStream;
     }
 
     public String getName() {
@@ -45,5 +77,9 @@ public class Player {
 
     public void setObjectOutputStream(ObjectOutputStream objectOutputStream) {
         this.objectOutputStream = objectOutputStream;
+    }
+
+    public void setObjectInputStream(ObjectInputStream objectInputStream) {
+        this.objectInputStream = objectInputStream;
     }
 }
